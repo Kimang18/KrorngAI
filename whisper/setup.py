d4c0e6daaad26816ed4842a208ae70ca7e9af3dc
Kimang18 <allenkhun22@gmail.com>
2025-12-14 12:41:44 +0700

    add more descriptions

--- whisper/setup.py
+++ whisper/setup.py
@@ -2,13 +2,27 @@
 # Date: December 2025
 
 
-from setuptools import setup, find_packages
+import sys
+from pathlib import Path
 
+from setuptools import setup, find_namespace_packages
+
+package_dir = Path(__file__).parent / 'neo_whisper'
+
+sys.path.append(str(package_dir))
 
 setup(
-    name='neo_whisper',
+    name='neo-whisper',
     version='0.0.1',
-    packages=find_packages(),
+    description='Improve Whisper with RoPE and latest tokenizers of OpenAI',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    readme="README.md",
+    author_email="kimang.khun@polytechnique.org",
+    author="KHUN Kimang",
+    url="https://github.com/kimang18/KrorngAI",
+    packages=find_namespace_packages(),
+    python_requires=">=3.8",
     install_requires=[
         # add dependencies here
         'requests>=2.25.1',
