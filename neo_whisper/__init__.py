ed2f8e26a9f3ed7fb8a87c6ab0de30d55565b725
Kimang18 <allenkhun22@gmail.com>
2025-12-14 12:26:23 +0700

    add original whisper but adapt to new tokenizer

---
+++ neo_whisper/__init__.py
@@ -0,0 +1,7 @@
+from whisper.decoding import DecodingResult, decode, detect_language
+
+from .tokenizer import get_tokenizer
+from .decoding import NeoDecodingOptions as DecodingOptions
+from .whisper import NeoWhisper
+from .whisper import NeoModelDimensions
+from .model import Whisper, ModelDimensions
