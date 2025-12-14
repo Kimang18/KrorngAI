860c6d95e31440469588df858d33398ccd102780
Kimang18 <allenkhun22@gmail.com>
2025-12-14 20:54:52 +0700

    remove dependencies required

--- whisper/setup.py
+++ whisper/setup.py
@@ -12,17 +12,20 @@ from setuptools import setup, find_namespace_packages
 package_dir = Path(__file__).parent / 'neo_whisper'
 
 # Path to common script outside the package root
-common_script_src = os.path.join("..", "common_utils", "nn_utils.py")
-common_script_dest = os.path.join(str(package_dir), "nn_utils.py")
-if os.path.exists(common_script_src):
-    shutil.copy(common_script_src, common_script_dest)
+for script in ["nn_utils.py", "common.py"]:
+    common_script_src = os.path.join("..", "common_utils", script)
+    common_script_dest = os.path.join(str(package_dir), script)
+    if os.path.exists(common_script_src):
+        shutil.copy(common_script_src, common_script_dest)
 
 sys.path.append(str(package_dir))
+
+from _version import __version__
 
 try:
     setup(
         name='neo-whisper',
-        version='0.0.1',
+        version=__version__,
         description='Improve Whisper with RoPE and latest tokenizers of OpenAI',
         long_description=open("README.md", encoding="utf-8").read(),
         long_description_content_type="text/markdown",
@@ -34,11 +37,13 @@ try:
         python_requires=">=3.8",
         install_requires=[
             # add dependencies here
-            'requests>=2.25.1',
+            # 'requests>=2.25.1',
             # GitHub dependency using PEP 508 URL format
-            'openai-whisper @ git+https://github.com/openai/whisper.git'
+            # 'openai-whisper @ git+https://github.com/openai/whisper.git'
         ]
     )
 finally:
-    if os.path.exists(common_script_dest):
-        os.remove(common_script_dest)
+    for script in ["nn_utils.py", "common.py"]:
+        common_script_dest = os.path.join(str(package_dir), script)
+        if os.path.exists(common_script_dest):
+            os.remove(common_script_dest)
