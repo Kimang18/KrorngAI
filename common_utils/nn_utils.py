ee7d362db1f2bd6868d006f7673a1ef1df67df55
Kimang18 <allenkhun22@gmail.com>
2025-12-18 22:31:00 +0700

    readjust lines only no change

--- common_utils/nn_utils.py
+++ common_utils/nn_utils.py
@@ -95,8 +95,7 @@ class Conv1D(nn.Module):
 
 class KVCache:
     def __init__(self, batch_size, num_heads, seq_len, head_dim, num_layers):
-        self.kv_shape = (num_layers, 2, batch_size,
-                         num_heads, seq_len, head_dim)
+        self.kv_shape = (num_layers, 2, batch_size, num_heads, seq_len, head_dim)
         self.kv_cache = None
         self.pos = 0
 
