e3671d5b67ca4164f2c55776a526a4cc6e07e516
Kimang18 <allenkhun22@gmail.com>
2026-04-22 13:45:22 +0200

    refactor code

--- tror-yong-asr/tror_yong_asr/__init__.py
+++ tror-yong-asr/tror_yong_asr/__init__.py
@@ -9,6 +9,13 @@ TaskOutput = namedtuple("TaskOutput", "output_ids text")
 
 
 def _task_handling(file_name: str, model: TrorYongASRModel, processor, task: str, max_tokens: int, temperature=1.0, top_k=None, seed=168, verbose=False) -> TaskOutput:
+    """
+    Args:
+    file_name: file path to audio (tested with mp3, but m4a or wav are not tested yet)
+    max_tokens: maximum number of tokens to be generated
+    temperature: calibrate randomness (e.g., 0.0 means greedy decoding)
+    verbose: if true and processor.language is None, then print the predicted language
+    """
     task_token_id = getattr(processor.tokenizer, task, None)
     if task_token_id is None:
         raise ValueError(f"No support for task {task}. Either 'transcribe' or 'translate' is supported.")
@@ -20,7 +27,9 @@ def _task_handling(file_name: str, model: TrorYongASRModel, processor, task: str
         resampler = torchaudio.transforms.Resample(sample_rate, 16000)
         waveform = resampler(waveform)
     audio_array = waveform.squeeze().numpy()  # 1D-array
+
     mels = processor(audio_array, sampling_rate=16000, return_tensors="pt").input_features[0]  # (80, 3000)
+
     if processor.tokenizer.language is None:
         lang_token_id, lang_probs = model.detect_language(mels, processor.tokenizer)
         if verbose:
@@ -28,7 +37,9 @@ def _task_handling(file_name: str, model: TrorYongASRModel, processor, task: str
         lang_token_id = lang_token_id.item()
     else:
         lang_token_id = processor.tokenizer.language_token
+
     model.set_prefix([processor.tokenizer.sot, lang_token_id, task_token_id])
+
     output_ids = model.decode(mels, max_tokens, temperature, top_k, seed)
     text = processor.decode(output_ids[0], skip_special_tokens=True)
     return TaskOutput(text=text, output_ids=output_ids[0])
